ca9ddf7261f354211dd57191b6076e7bac4393d0
Hilary Titus Naor <hilarytn@gmail.com>
2023-04-22 22:41:56 +0000

    Add lines

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Flask Application to display 'Hello HBNB' """
+"""Flask Application to display 'Hello HBNB' """i
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
