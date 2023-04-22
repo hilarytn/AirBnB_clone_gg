ccd610a67605ea75faee4c0d8df86e03c259059e
Hilary Titus Naor <hilarytn@gmail.com>
2023-04-22 22:43:57 +0000

    Add lines

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Flask Application to display 'Hello HBNB' """i
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
-@app.route("/", strict_slashes=False)
-def display():
-    return """Hello HBNB!"""
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000, debug=None)
